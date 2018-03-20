6b9b48469fffb2f5b74b7797e374c2cf3dcaf8a3
PreeduS <preedus@gmail.com>
2018-03-20 11:43:46 +0200

    Updated routes db

--- routes/db.js
+++ routes/db.js
@@ -4,17 +4,50 @@ var router = express.Router();
 const Article = require('../models/article');
 
 router.get("/addArticle",(req,res)=>{ 
-    res.send(req.path); 
-})
+    let article = new Article();
+
+    let r = Math.random().toFixed(2);
+    article.title = `title ${r}`;
+    article.author = `author ${r}`;
+    article.body = `body ${r}`;
+
+    article.save(err=>{
+        if(err){
+            return res.send('err: '+err); 
+        }
+        return res.send(`data ${r} added`);
+    });
+    
+});
 
 router.get("/findArticles",(req,res)=>{ 
     Article.find({},(err, articles)=>{
         if(err){ return res.send(err); }
-        res.send('articles: '+articles)
+       
+        let r = articles.reduce((acc,val)=> {
+            let del = `<a href="/db/deleteArticle/${val._id}">[d]</a>`;
+            return acc + `title: ${val.title},  author: ${val.author}, body: ${val.body} ${del}<br />` 
+        },'')
+        res.send(r)
     });
     //res.send(req.path); 
-})
+});
+router.get("/deleteArticle/:id",(req,res)=>{ 
+    let id = req.params.id;
+    Article.remove({_id:id},err=>{
+        if(err){
+            return res.send('err: '+err);
+        }
+        res.send(id + ' removed');
+    });
+
+
+});
 
 
 module.exports = router;
 
+/*
+article.update({_id:"..."}, newObject,  err=>{...})
+article.update({ _id: id }, { $set: { size: 'large' }}, callback);
+*/
